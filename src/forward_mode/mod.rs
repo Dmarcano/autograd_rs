785c931b7e57c995141e94880f7aa8fded2a48ca
Diego Marcano <marcanp.diego@gmail.com>
2021-08-11 01:07:19 -0400

    small impl of forward mode ad

--- src/forward_mode/mod.rs
+++ src/forward_mode/mod.rs
@@ -3,7 +3,7 @@
 
 use std::ops::{Add, Mul, Div, Sub};
 
-
+#[derive(Copy, Clone, Debug)]
 pub struct ForwardTensor { 
     pub data : f32, 
     pub deriv : f32,  
@@ -14,8 +14,15 @@ impl Add for ForwardTensor {
     type Output = ForwardTensor;
 
     
-    fn add(self, _: ForwardTensor) -> Self::Output {
-         todo!() 
+    fn add(self, other: ForwardTensor) -> Self::Output {
+        let data = self.data + other.data; 
+        let deriv = self.deriv + other.deriv;
+
+        if self.debug { 
+            println!("Performing Tensort Addition with {} {} \nwith derivatives {} {} \nwhich outputs: data {}  derivative {}", self.data, other.data, self.deriv, other.deriv, data, deriv);
+        }
+
+         ForwardTensor { data, deriv , debug: self.debug }
     }
 }
 
@@ -23,8 +30,16 @@ impl Sub for ForwardTensor {
     type Output = ForwardTensor;
 
     
-    fn sub(self, _: ForwardTensor) -> Self::Output {
-         todo!() 
+    fn sub(self, other:  ForwardTensor) -> Self::Output {
+        let data = self.data - other.data; 
+        let deriv = self.deriv - other.deriv;
+
+        if self.debug { 
+            println!("Performing Tensort Subtraction with {} {} \nwith derivatives {} {} \nwhich outputs: data {}  derivative {}", self.data, other.data, self.deriv, other.deriv, data, deriv);
+        }
+
+         ForwardTensor { data, deriv , debug: self.debug }
+
     }
 }
 
@@ -33,8 +48,15 @@ impl Mul for ForwardTensor {
     type Output = ForwardTensor;
 
     
-    fn mul(self, _: ForwardTensor) -> Self::Output {
-         todo!() 
+    fn mul(self, other: ForwardTensor) -> Self::Output {
+        let data = self.data - other.data; 
+        let deriv = self.deriv * other.data + self.data * other.deriv;
+
+        if self.debug { 
+            println!("Performing Tensort Multiplication with {} {} \nwith derivatives {} {} \nwhich outputs: data {}  derivative {}", self.data, other.data, self.deriv, other.deriv, data, deriv);
+        }
+
+         ForwardTensor { data, deriv , debug: self.debug }
     }
 }
 
@@ -42,19 +64,41 @@ impl Div for ForwardTensor {
     type Output = ForwardTensor;
 
     
-    fn div(self, _: ForwardTensor) -> Self::Output {
-         todo!() 
+    fn div(self, other: ForwardTensor) -> Self::Output {
+        let data = self.data - other.data; 
+        let deriv = (self.deriv * other.data - self.data * other.deriv) / (other.data * other.data);
+
+        if self.debug { 
+            println!("Performing Tensort Division with {} {} \nwith derivatives {} {} \nwhich outputs: data {}  derivative {}", self.data, other.data, self.deriv, other.deriv, data, deriv);
+        }
+
+         ForwardTensor { data, deriv , debug: self.debug }
     }
 }
 
 impl ForwardTensor { 
-
+    
     pub fn exp(self) -> Self { 
-        todo!()
+        let data = self.data.exp(); 
+        let deriv = data * self.deriv;
+
+        if self.debug { 
+            println!("Performing Tensort exp with {}  \nwith derivative {}  \nwhich outputs: data {}  derivative {}", self.data, self.deriv, data, deriv);
+        }
+
+         ForwardTensor { data, deriv , debug: self.debug }
+
     }
 
     pub fn sin(self) -> Self { 
-        todo!()
+        let data = self.data.sin(); 
+        let deriv = self.data.cos() * self.deriv;
+
+        if self.debug { 
+            println!("Performing Tensort exp with {}  \nwith derivative {}  \nwhich outputs: data {}  derivative {}", self.data, self.deriv, data, deriv);
+        }
+
+         ForwardTensor { data, deriv , debug: self.debug }
     }
 }
 
@@ -62,7 +106,32 @@ impl ForwardTensor {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn add_test() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn sub_test() {
+        unimplemented!();
+    }
+
+    #[test]
+    fn mul_test() {
+        unimplemented!();
+    }
+
+    #[test]
+    fn div_test() {
+        unimplemented!();
+    }
+
+    #[test]
+    fn exp_test() {
+        unimplemented!();
+    }
+
+    #[test]
+    fn sin_test() {
+        unimplemented!();
     }
 }
